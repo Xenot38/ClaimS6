c27a90d41278329dc3a9d1c6b6387facf056ada3
baratinm <baratinm@f212-08.e-im2ag.ujf-grenoble.fr>
2019-05-09 15:22:08 +0200

    passage de joue dans JoueurHumain

--- Claim/src/controler/ControleurJeu.java
+++ Claim/src/controler/ControleurJeu.java
@@ -31,26 +31,22 @@ public class ControleurJeu {
                                 Carte maCarte = iter.next();
                                 System.out.println(nbCarte + ": " + maCarte.affichePropCarte());
                                 nbCarte++;
-                        }
-                        
-                        System.out.println("\n\n[Main joueur 2]");
+                        } 
+                        /*System.out.println("\n\n[Main joueur 2]");
                         iter = plateauJeu.getJ2().getMain().iterator();
                         nbCarte = 0;
                         while(iter.hasNext()){
                                 Carte maCarte = iter.next();
                                 System.out.println(nbCarte + ": " + maCarte.affichePropCarte());
                                 nbCarte++;
-                        }
+                        }*/
                         if(plateauJeu.getPhase() == 1){
                                 plateauJeu.setCarteEnJeu(plateauJeu.getPioche().pop());
                                 System.out.println("La carte en jeu est : " + plateauJeu.getCarteEnJeu().affichePropCarte());
                         }
                         //System.out.println("entrez l'index de la carte a jouer (0-indexé)");
                         if(plateauJeu.isJ1Courant()){           //Si le J1 a la main, il joue en premier et les choix du J2 sont restreints en conséquence.
-                                Scanner sc = new Scanner(System.in);
-                                System.out.println("Veuillez saisir un nombre :");
-                                int str = sc.nextInt();
-                                plateauJeu.setCarteJ1(plateauJeu.getJ1().choisirCarte(str));
+                                plateauJeu.setCarteJ1(plateauJeu.getJ1().choisirCarte(plateauJeu.getJ1().joue(plateauJeu)));
                                 System.out.println("Vous avez joué " + plateauJeu.getCarteJ1().affichePropCarte());
                                 plateauJeu.setCarteJ2(plateauJeu.getJ2().choisirCarte(plateauJeu.getJ2().joue(plateauJeu),plateauJeu.getCarteJ1()));
                                 System.out.println("L'adversaire a joué " + plateauJeu.getCarteJ2().affichePropCarte());
@@ -58,15 +54,7 @@ public class ControleurJeu {
                         }else{
                                 plateauJeu.setCarteJ2(plateauJeu.getJ2().choisirCarte(plateauJeu.getJ2().joue(plateauJeu)));
                                 System.out.println("L'adversaire a joué " + plateauJeu.getCarteJ2().affichePropCarte());
-                                while(plateauJeu.getCarteJ1()==null){//Si le J2 a la main, il joue en premier et les choix du J1 tant que l'utilisateur n'a pas choisi une carte viable, on lui redemande de choisir une carte.
-                                        Scanner sc = new Scanner(System.in);
-                                        System.out.println("Veuillez saisir un nombre :");
-                                        int str = sc.nextInt();
-                                        plateauJeu.setCarteJ1(plateauJeu.getJ1().choisirCarte(str,plateauJeu.getCarteJ2()));
-                                        if(plateauJeu.getCarteJ1() == null){
-                                                System.out.println("Cette carte n'est pas jouable dans cette situation, choisissez en une autre.");
-                                        }
-                                }
+                                plateauJeu.setCarteJ1(plateauJeu.getJ1().choisirCarte(plateauJeu.getJ1().joue(plateauJeu)));
                                 System.out.println("Vous avez joué " + plateauJeu.getCarteJ1().affichePropCarte());
                         }
                         plateauJeu.calculPli();
--- Claim/src/model/JoueurHumain.java
+++ Claim/src/model/JoueurHumain.java
@@ -1,12 +1,41 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class JoueurHumain extends Joueur{	
 	
 	public JoueurHumain(ArrayList<Carte> premiereMain){
 		super(premiereMain);		
 	}
+        @Override
+        public int joue(Plateau p){
+                int str = -1;
+                Scanner sc = new Scanner(System.in);
+                while(str==-1){
+                        System.out.println("Veuillez saisir un nombre :");
+                        int temp = sc.nextInt();
+                        if(temp>=this.getMain().size()||temp<0){
+                                System.out.println(temp + " n'est pas une valeur possible, veuillez choisir entre 0 et " + (this.getMain().size()-1));
+                        }else{ 
+                                if(p.getCarteJ2()==null){
+                                        str = temp;
+                                }else{
+                                        if(this.getCartesJouable(p.getCarteJ2()).contains(this.getMain().get(temp))){
+                                                str = temp;
+                                        }else{
+                                                System.out.println("Cette carte ne peut pas etre jouée, choisissez parmis les suivantes :");
+                                                Iterator<Carte> ite = this.getCartesJouable(p.getCarteJ2()).iterator();
+                                                while(ite.hasNext()){
+                                                        System.out.println(ite.next().affichePropCarte());
+                                                }
+                                        }
+                                }
+                        }
+                }
+                return str;
+        }
 	
 	
 }
